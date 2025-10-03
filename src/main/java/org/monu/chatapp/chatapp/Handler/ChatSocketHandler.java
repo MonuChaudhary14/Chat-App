abf2d1557f38c9fa21a94ec7513ab3667da55ea4
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-03 18:08:03 +0530

    WebSocket Configuration class Added

---
+++ src/main/java/org/monu/chatapp/chatapp/Handler/ChatSocketHandler.java
@@ -0,0 +1,12 @@
+package org.monu.chatapp.chatapp.Handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class ChatSocketHandler extends TextWebSocketHandler {
+
+
+
+
+}
---
+++ src/main/java/org/monu/chatapp/chatapp/configuration/WebSocket.java
@@ -0,0 +1,21 @@
+package org.monu.chatapp.chatapp.configuration;
+
+import org.monu.chatapp.chatapp.Handler.ChatSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocket implements WebSocketConfigurer {
+
+    @Autowired
+    private ChatSocketHandler chatSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatSocketHandler , "/chat").setAllowedOrigins("*");
+    }
+}
