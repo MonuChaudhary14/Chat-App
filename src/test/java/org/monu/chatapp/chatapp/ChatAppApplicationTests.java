f8e0592dd9a6d47abe4c8bbe7c2d2c7059a1132c
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-03 15:53:29 +0530

    CICD Bug fixed

--- src/test/java/org/monu/chatapp/chatapp/ChatAppApplicationTests.java
+++ src/test/java/org/monu/chatapp/chatapp/ChatAppApplicationTests.java
@@ -2,6 +2,7 @@ package org.monu.chatapp.chatapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class ChatAppApplicationTests {
